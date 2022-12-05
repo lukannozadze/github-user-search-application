036ae5b225fdf64d275c38a38edd50fb34b00261
Luka <nozadzeluka2002@gmail.com>
2022-12-05 22:58:34 +0400

    fix error visualisation on mobile and table versions 3.0

--- src/Components/Search Input/SearchInput.jsx
+++ src/Components/Search Input/SearchInput.jsx
@@ -28,8 +28,8 @@ const SearchInput = (props) => {
           </p>
         ) : !props.isValid ? (
           <p
-            className="absolute right-[120px] top-[23px] text-[#F74646] font-bold text-[15px]  def:block def:text-[13px] def:top-[22px] def:right-[100px] 
-          sm:text-[15px] sm:right-[120px] sm:top-[23px]"
+            className="absolute right-[120px] top-[23px] text-[#F74646] font-bold text-[15px]  def:block def:text-[13px] def:top-[23px] def:right-[100px] 
+          sm:text-[15px] sm:right-[120px] sm:top-[24gpx]"
           >
             No results
           </p>
